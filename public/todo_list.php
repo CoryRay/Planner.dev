0481e5b00ac3bca8d97edfa76d1c960f0b92de16
Cory Rodriguez <coreyreylp@gmail.com>
2014-08-25 16:00:12 -0500

    todo list template added

---
+++ public/todo_list.php
@@ -0,0 +1,14 @@
+<html>
+	<head>
+		<title>Todo List</title>
+	</head>
+	<body>
+		<h1>$_GET</h1>
+		<?php var_dump($_GET); ?>
+
+		<h1>$_POST</h1>
+		<?php var_dump($_POST); ?>
+
+		<h1>Todo List</h1>
+	</body>
+</html>
