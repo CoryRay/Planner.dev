098f1e84f9d46a998c8c4154ab2cafb5fe4d8959
Cory Rodriguez <coreyreylp@gmail.com>
2014-08-26 15:23:22 -0500

    added some style

--- public/todo_list.php
+++ public/todo_list.php
@@ -2,33 +2,38 @@
 <html>
 	<head>
 		<meta charset="utf-8">
+		<link type="text/css" rel="stylesheet" href="css/todo_list.css"/>
 		<title>Todo List</title>
 	</head>
 	<body>
-		<h1>$_GET</h1>
-		<?php var_dump($_GET); ?>
+		<div>	
+			<h1>$_GET</h1>
+			<?php var_dump($_GET); ?>
 
-		<h1>$_POST</h1>
-		<?php var_dump($_POST); ?>
+			<h1>$_POST</h1>
+			<?php var_dump($_POST); ?>
 
-		<h1>Todo List</h1>
-		<ul>
-			<li>Take the dog out</li>
-			<li>Place mom's spaghetti on the cat</li>
-			<li>Water the cat</li>
-			<li>Rub Buddha's belly for luck</li>
-			<li><?php foreach ($_POST as $key => $value) {
-				echo $value;
-			}?></li>
-		</ul>
+			<h2>Todo List</h2>
+			<ul>
+				<li>Take the dog out</li>
+				<li>Place mom's spaghetti on the cat</li>
+				<li>Water the cat</li>
+				<li>Rub Buddha's belly for luck</li>
+					<?php foreach ($_POST as $key => $value) {
+					echo "<li>" . $value . "</li>";
+					// $value[] = $_POST
+				}?>
+			</ul>
 
-		<h2>Form</h2>
+			<h2>Form</h2>
 
-		<form method="POST" action="todo_list.php">
-			<p>
-				<label for="newItem">Enter new todo item:</label>
-				<input type="text" id="newItem" name="newItem">
-			</p>
-			<button>Add Item</button>
-		</form>
+			<form method="POST" action="todo_list.php">
+				<p>
+					<label for="newItem">Enter new todo item [limit one]:</label>
+					<input type="text" id="newItem" name="newItem">
+				</p>
+				<button>Add Item</button>
+			</form>
+		</div>
+	</body>	
 </html>
