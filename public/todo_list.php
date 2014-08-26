fd73d5be66f9726c2e082eccd04be5e3f33db6b2
Cory Rodriguez <coreyreylp@gmail.com>
2014-08-26 10:11:34 -0500

    completed exercise

--- public/todo_list.php
+++ public/todo_list.php
@@ -1,5 +1,7 @@
+<!DOCTYPE html>
 <html>
 	<head>
+		<meta charset="utf-8">
 		<title>Todo List</title>
 	</head>
 	<body>
@@ -10,5 +12,23 @@
 		<?php var_dump($_POST); ?>
 
 		<h1>Todo List</h1>
-	</body>
+		<ul>
+			<li>Take the dog out</li>
+			<li>Place mom's spaghetti on the cat</li>
+			<li>Water the cat</li>
+			<li>Rub Buddha's belly for luck</li>
+			<li><?php foreach ($_POST as $key => $value) {
+				echo $value;
+			}?></li>
+		</ul>
+
+		<h2>Form</h2>
+
+		<form method="POST" action="todo_list.php">
+			<p>
+				<label for="newItem">Enter new todo item:</label>
+				<input type="text" id="newItem" name="newItem">
+			</p>
+			<button>Add Item</button>
+		</form>
 </html>
