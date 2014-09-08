98577e58711e1270b6f5b1e5cfd56cc0c6ab2071
Cory Rodriguez <coreyreylp@gmail.com>
2014-09-08 16:40:35 -0500

    Started working on todo with php integrated

--- public/todo_list.php
+++ public/todo_list.php
@@ -2,11 +2,12 @@
 <html>
 	<head>
 		<meta charset="utf-8">
+		<link type="text/css" rel="stylesheet" href="bootstrap/css/bootstrap.min.css"/>
 		<link type="text/css" rel="stylesheet" href="css/todo_list.css"/>
 		<title>Todo List</title>
 	</head>
 	<body>
-		<div>	
+		<div class='container-fluid'>	
 			<h1>$_GET</h1>
 			<?php var_dump($_GET); ?>
 
@@ -14,18 +15,37 @@
 			<?php var_dump($_POST); ?>
 
 			<h2>Todo List</h2>
-			<ul>
-				<li>Take the dog out</li>
-				<li>Place mom's spaghetti on the cat</li>
-				<li>Water the cat</li>
-				<li>Rub Buddha's belly for luck</li>
-					<?php foreach ($_POST as $key => $value) {
-					echo "<li>" . $value . "</li>";
-					// $value[] = $_POST
-				}?>
-			</ul>
 
-			<h2>Form</h2>
+			<ol>
+ 					<?php
+
+ 				    function writeToFile() {
+
+ 				    };
+
+					$todo_items = [ //Original html hardcoded array
+						'Place mom\'s spaghetti on the cat',
+						'Water the cat',
+						'Rub Buddha\'s belly for luck'
+					];
+
+					//opens txt file and merges it with original array
+ 				    $addl_items = file('data/todo_list.txt', FILE_IGNORE_NEW_LINES | FILE_SKIP_EMPTY_LINES);
+ 				        $todo_items = array_merge($todo_items, $addl_items);
+
+					foreach ($todo_items as $value) { // echoes out the original array
+						echo "<li>" . $value . "</li>";
+					};
+
+					foreach ($_POST as $key => $value) { //echoes out 
+						echo "<li>" . $value . "</li>";
+						// $value[] = $_POST;
+					};
+
+					?>
+			</ol>
+
+			<h2>Add Item</h2>
 
 			<form method="POST" action="todo_list.php">
 				<p>
